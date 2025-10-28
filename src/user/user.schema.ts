c1db6224d13f0df8431d2b81e1d66663087529cb
Nguyen Truong Ngoc Han <23520434@gm.uit.edu.vn>
2025-10-28 13:33:58 +0700

    feat(auth): align with driver module and add role propagation

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,22 +1,17 @@
-import {Schema, Prop, SchemaFactory} from '@nestjs/mongoose';
-import {Document} from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-@Schema()
-export class User extends Document {
-    @Prop({required: true, unique: true})
-    email: string;
-    
-    @Prop({required: true, unique: true})
-    phone: string;
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ unique: true, required: true }) email: string;
+  @Prop() phone?: string;
 
-    @Prop({required: true})
-    password: string;
+  @Prop({ default: false }) isDriver: boolean;
+  @Prop({ default: false }) isEmailVerified: boolean;
 
-    @Prop({default: false})
-    isEmailVerified: boolean;
+  @Prop({ unique: true, sparse: true }) cognitoSub?: string;
 
-    @Prop({default: false})
-    isDriver: boolean;
+  @Prop() password?: string;
 }
-
+export type UserDocument = User & Document;
 export const UserSchema = SchemaFactory.createForClass(User);
